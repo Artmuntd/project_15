57fe6229b0b79a5d2752a992ea2f336ac6d6ccea
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2021-01-13 22:28:03 +0300

    few changes

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.use(helmet());
 
 app.use(requestLogger);
 
-app.post('/signin', celebrate({
+app.post('api/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
-app.post('/signup', celebrate({
+app.post('api/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
@@ -45,12 +45,12 @@ app.post('/signup', celebrate({
 }), createUser);
 app.use(auth);
 
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use('api/users', usersRouter);
+app.use('api/cards', cardsRouter);
 
 app.use(errorLogger);
 
-app.get('/crash-test', () => {
+app.get('api/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
